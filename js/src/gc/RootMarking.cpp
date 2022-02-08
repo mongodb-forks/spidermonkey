b25396d84a561aadda5b937a4af9bd2e628fe897
Justin Seyster <justin.seyster@mongodb.com>
2022-02-08 14:22:57 -0500

    Do not mark ConcreteTraceable::trace() as deleted in MSVC

--- js/src/gc/RootMarking.cpp
+++ js/src/gc/RootMarking.cpp
@@ -48,7 +48,16 @@ using RootEnum = RootedValueMap::Enum;
 // ConcreteTraceable type are actually used at runtime.
 struct ConcreteTraceable {
   ConcreteTraceable() = delete;
-  void trace(JSTracer*) { MOZ_CRASH("This path is unreachable."); }
+#ifdef _MSC_VER
+  // There should be no callers to ConcreteTraceable::trace(). In clang/gcc, it
+  // is possible to ensure that using the "= delete" syntax, but MSVC is more
+  // aggressive about what is considered a caller.
+  void trace(JSTracer*) {
+    MOZ_ASSERT_UNREACHABLE();
+  }
+#else
+  void trace(JSTracer*) = delete;
+#endif
 };
 
 template <typename T>
