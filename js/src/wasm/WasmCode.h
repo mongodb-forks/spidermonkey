9d228d84a2b7d84901035dacaeca535d53bb969c
Justin Seyster <justin.seyster@mongodb.com>
2022-02-07 19:14:05 -0500

    Add MSVC alternative to __atomic_compare_exchange_n

--- js/src/wasm/WasmCode.h
+++ js/src/wasm/WasmCode.h
@@ -905,16 +905,7 @@ class JumpTables {
     MOZ_ASSERT(i < numFuncs_);
     __atomic_store_n(&jit_.get()[i], target, __ATOMIC_RELAXED);
   }
-  void setJitEntryIfNull(size_t i, void* target) const {
-    // Make sure that compare-and-write is atomic; see comment in
-    // wasm::Module::finishTier2 to that effect.
-    MOZ_ASSERT(i < numFuncs_);
-    void* expected = nullptr;
-    (void)__atomic_compare_exchange_n(&jit_.get()[i], &expected, target,
-                                      /*weak=*/false,
-                                      /*success_memorder=*/__ATOMIC_RELAXED,
-                                      /*failure_memorder=*/__ATOMIC_RELAXED);
-  }
+  void setJitEntryIfNull(size_t i, void* target) const;
   void** getAddressOfJitEntry(size_t i) const {
     MOZ_ASSERT(i < numFuncs_);
     MOZ_ASSERT(jit_.get()[i]);
