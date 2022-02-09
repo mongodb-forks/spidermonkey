a45b352634c841e8423418508d56fc48acecb5f3
Justin Seyster <justin.seyster@mongodb.com>
2022-02-08 20:10:11 -0500

    Fall back to switch-based interpreters in MSVC
    Mozjs has two bytecode interpreters, each of which operates in
    "threaded" mode* by default: one for the Javascript VM and one for the
    regular expression state machine (imported from V8).
    
    In a "threaded" interpreter, each instruction has a label; an array
    stores the address of each label, indexed by the instruction opcode;
    and each instruction finishes its execution by looking up the label
    for the next instruction and jumping to that label with a "computed
    goto."
    
    This style of interpreter requires special && syntax (no relation to
    r-value references) to get the address for each label, as well as
    support for using a runtime value as the target for a goto
    statement. These are supported as extensions in Clang and gcc, but a
    fallback switch-based approach is necessary for MSVC, which does not
    support these extensions.
    
    Preprocessor magic makes it possible to write a single interpreter
    that can use either approach, depending on a small set of macro
    definitions. The V8 regex interpreter already supports both
    approaches, and this patch chooses the fallback when compiling on
    Windows. The mozjs Javascript interpreter supported a switch-based
    fallback in a _previous_ release, but that fallback was removed,
    because none of mozjs's officially supported compilers need it. This
    path reverses that removal, porting the switch-based interpreter
    forward to this release.
    
    *Note: "threaded" here does not refer to multi-threaded execution. The
    interpreter still runs on a single thread.

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -1882,12 +1882,14 @@ void js::ReportInNotObjectError(JSContext* cx, HandleValue lref,
 
 static MOZ_NEVER_INLINE JS_HAZ_JSNATIVE_CALLER bool Interpret(JSContext* cx,
                                                               RunState& state) {
+#ifndef NO_COMPUTED_GOTO
 /*
  * Define macros for an interpreter loop. Opcode dispatch is done by
  * indirect goto (aka a threaded interpreter), which is technically
- * non-standard but is supported by all of our supported compilers.
+ * non-standard but is supported by many compilers.
  */
 #define INTERPRETER_LOOP()
+#define CASE_NOT_COMPUTED(OP) label_##OP:
 #define CASE(OP) label_##OP:
 #define DEFAULT() \
   label_default:
@@ -1907,6 +1909,28 @@ static MOZ_NEVER_INLINE JS_HAZ_JSNATIVE_CALLER bool Interpret(JSContext* cx,
           FOR_EACH_TRAILING_UNUSED_OPCODE(TRAILING_LABEL)
 #undef TRAILING_LABEL
   };
+#else  // End of case where computed goto is available.
+/*
+ * Define macros for a switch-based interpreter loop. Using a switch statement
+ * for dispatch is usually not as fast as the "threaded" approach that uses
+ * indirect goto statements, but it does not require any language extensions
+ * and should work on any standard-compliant C++ compiler.
+ */
+#define INTERPRETER_LOOP()   \
+  the_switch:                \
+    switch (switchOp)
+#define CASE_NOT_COMPUTED(OP) case OP:
+#define CASE(OP) case static_cast<jsbytecode>(JSOp::OP):
+#define DEFAULT() default:
+#define DISPATCH_TO(OP)   \
+    JS_BEGIN_MACRO        \
+      switchOp = (OP);    \
+      goto the_switch;    \
+    JS_END_MACRO
+
+  // This variable is effectively a parameter to the switch.
+  jsbytecode switchOp;
+#endif  // End of case where computed goto is _not_ available.
 
   /*
    * Increment REGS.pc by N, load the opcode at that position,
@@ -2083,7 +2107,7 @@ static MOZ_NEVER_INLINE JS_HAZ_JSNATIVE_CALLER bool Interpret(JSContext* cx,
   ADVANCE_AND_DISPATCH(0);
 
   INTERPRETER_LOOP() {
-    CASE(EnableInterruptsPseudoOpcode) {
+    CASE_NOT_COMPUTED(EnableInterruptsPseudoOpcode) {
       bool moreInterrupts = false;
       jsbytecode op = *REGS.pc;
 
