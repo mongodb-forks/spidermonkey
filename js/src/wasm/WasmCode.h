e024594fd5d5bedb05f71984e68a9fd2c08023e9
Justin Seyster <justin.seyster@mongodb.com>
2022-02-16 22:45:56 -0500

    Moved setJitEntryIfNull to its own file

--- js/src/wasm/WasmCode.h
+++ js/src/wasm/WasmCode.h
@@ -663,23 +663,7 @@ class JumpTables {
     MOZ_ASSERT(i < numFuncs_);
     jit_.get()[i] = target;
   }
-  void setJitEntryIfNull(size_t i, void* target) const {
-    // Make sure that compare-and-write is atomic; see comment in
-    // wasm::Module::finishTier2 to that effect.
-    MOZ_ASSERT(i < numFuncs_);
-
-#ifdef _MSC_VER
-    int64_t expected = 0;
-    (void)_InterlockedCompareExchange64_np(reinterpret_cast<intptr_t*>(&jit_.get()[i]),
-                                           reinterpret_cast<intptr_t>(target),
-                                           expected);
-#else
-    void* expected = nullptr;
-    (void)__atomic_compare_exchange_n(&jit_.get()[i], &expected, target,
-                                      /*weak=*/false, __ATOMIC_RELAXED,
-                                      __ATOMIC_RELAXED);
-#endif
-  }
+  void setJitEntryIfNull(size_t i, void* target) const;
   void** getAddressOfJitEntry(size_t i) const {
     MOZ_ASSERT(i < numFuncs_);
     MOZ_ASSERT(jit_.get()[i]);
