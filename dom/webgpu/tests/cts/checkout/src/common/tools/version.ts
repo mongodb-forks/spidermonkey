d8b369cc019be0b61adb19025e80f8fe9379965a
Santiago Roche <santiago.roche@mongodb.com>
2024-02-07 20:44:18 +0000

    Removed all Non-SpiderMonkey Files

--- dom/webgpu/tests/cts/checkout/src/common/tools/version.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable-next-line n/no-restricted-require */
-export const version = require('child_process')
-  .execSync('git describe --always --abbrev=0 --dirty')
-  .toString()
-  .trim();
