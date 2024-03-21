32e830b6b0c9adb8a47d5d64a43a8c6eae0956c9
Santiago Roche <santiago.roche@mongodb.com>
2024-03-21 17:43:24 +0000

    Make gc::Cell.h operator delete protected.

--- js/src/gc/Cell.h
+++ js/src/gc/Cell.h
@@ -227,7 +227,6 @@ struct Cell {
  protected:
   uintptr_t address() const;
 
- private:
   // Cells are destroyed by the GC. Do not delete them directly.
   void operator delete(void*) = delete;
 } JS_HAZ_GC_THING;
