52f5cf8049e273a03041c5cc22f1a446b4bc4bc4
Erin Zhu <erin.zhu@mongodb.com>
2025-08-11 17:30:31 -0400

    Disable test262 cases which use Intl.DurationFormat API

--- js/src/tests/test262/intl402/DurationFormat/prototype/formatToParts/negative-duration-formatToParts-style-narrow-en.js
+++ js/src/tests/test262/intl402/DurationFormat/prototype/formatToParts/negative-duration-formatToParts-style-narrow-en.js
@@ -1,4 +1,4 @@
-// |reftest| skip-if(!Intl.hasOwnProperty('DurationFormat')) -- Intl.DurationFormat is not enabled unconditionally
+// |reftest| skip-if(!this.hasOwnProperty("Intl")||!Intl.hasOwnProperty('DurationFormat')) -- Intl.DurationFormat is not enabled unconditionally
 // Copyright (C) 2023 André Bargull. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
