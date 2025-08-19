129269900cfabb6c31509587915ca2ec8bf65777
Erin Zhu <erin.zhu@mongodb.com>
2025-08-19 11:32:10 -0400

    Move placement new call to resolve compilation errors for GCC for accessing protected functions

--- js/src/gc/BufferAllocator.cpp
+++ js/src/gc/BufferAllocator.cpp
@@ -1598,7 +1598,8 @@ void* BufferAllocator::allocSmall(size_t bytes, bool nurseryOwned) {
     return nullptr;
   }
 
-  auto* cell = new (ptr) SmallBuffer();
+  //MONGODB MODIFICATION: Moves placement new call within SmallBuffer to resolve compilation error using GCC.
+  auto* cell = SmallBuffer::create(ptr);
   cell->setNurseryOwned(nurseryOwned);
   MOZ_ASSERT(cell->isNurseryOwned() == nurseryOwned);
   void* alloc = cell->data();
@@ -1619,7 +1620,8 @@ void* BufferAllocator::allocSmallInGC(size_t bytes, bool nurseryOwned) {
     return nullptr;
   }
 
-  auto* cell = new (ptr) SmallBuffer();
+  //MONGODB MODIFICATION: Moves placement new call within SmallBuffer to resolve compilation error using GCC.
+  auto* cell = SmallBuffer::create(ptr);;
   cell->setNurseryOwned(nurseryOwned);
   void* alloc = cell->data();
 
