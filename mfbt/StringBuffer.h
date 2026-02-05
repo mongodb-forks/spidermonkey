6f0a9ceb784468d17ebd615f6a440200c2c8ab15
Aditya Deshpande <aditya.deshpande@mongodb.com>
2026-02-05 17:23:47 +0000

    Address review comments and remove .github dir

--- mfbt/StringBuffer.h
+++ mfbt/StringBuffer.h
@@ -17,6 +17,7 @@
 #include "mozilla/RefCounted.h"
 #include "mozmemory.h"
 // MONGODB MODIFICATION: Explicitly include Utility.h for arena allocation functions.
+// Needed for backports to 8.0 and earlier due to clang-format header ordering changes (SERVER-105050).
 #include "js/Utility.h"
 
 namespace mozilla {
